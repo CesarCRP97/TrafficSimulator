188f1ccde0864185ec5c9bb268f7a091ac88a4c6
samuelrdgz <61139116+samuelrdgz@users.noreply.github.com>
2020-02-26 12:27:48 +0100

    Prueba 2

--- src/simulator/model/Road.java
+++ src/simulator/model/Road.java
@@ -3,65 +3,8 @@ package simulator.model;
 import java.util.List;
 
 abstract public class Road extends SimulatedObject{
-	Junction cruce_orig;
-	Junction cruce_dest;
-	int longitud;
-	int vel_max;
-	int limite_act;
-	int alarm;
-	Weather cond_amb;
-	int total_contamination;
-	private List <Vehicle> vehicle;
 	
-	
-
-	Road(String id, Junction srcJunc,  Junction destJunc, int maxSpeed, int contLimit, int length, Weather weather){
-			super(id);
-		// TODO Auto-generated constructor stub
-	}
-	
-	void enter (Vehicle v) throws Exception {
-		
-	}
-
-	
-	void exit (Vehicle v) { this.vehicle.remove(v); }
-	
-	
-	void setWeather (Weather w)  {
-		
-	}
-	
-	
-	void addContamination (int c) {
-	}
-	
-	
-	abstract void reduceTotalContamination ();
-	
-	abstract void updateSpeedLimit (); 
-	
-	abstract int calculateVehicleSpeed (Vehicle v);
-	
-	@Override
-	void advance(int time) {
-		// TODO Auto-generated method stub
-	}
-
-	
-	@Override
-	public JSONObject report() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	
-	
-	
-	
-	
-	
-   /* private Junction src;
+    private Junction src;
     private Junction dest;
     private int length;         //Longitud
     private int maxSpeed;
@@ -111,7 +54,8 @@ abstract public class Road extends SimulatedObject{
     //Abstract methods
     abstract void reduceTotalContamination();
     abstract void updateSpeedLimit();
-    abstract int calculateVehicleSpeed(Vehicle v);*/
+    abstract int calculateVehicleSpeed(Vehicle v);
 }
+
 
 
