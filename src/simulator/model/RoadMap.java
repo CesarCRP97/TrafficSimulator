6235f4f3eb6d206e644a14b401bb0aa83bf8e507
ladronblanco <ccrp97@hotmail.es>
2020-03-09 12:45:00 +0100

    report methods from Road and Vehicle and roadMap methods added

--- src/simulator/model/RoadMap.java
+++ src/simulator/model/RoadMap.java
@@ -2,32 +2,32 @@ package simulator.model;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.function.Consumer;
 
 public class RoadMap {
-	protected List<Junction> junct;
+	protected List<Junction> junctions;
 	protected List<Road> roads;
-	protected List<Vehicle> ve;
-	protected Map<String,Junction> junctionMap;
-	protected Map<String,Road> roadsMap;
-	protected Map<String,Vehicle> vehiclesMap;
+	protected List<Vehicle> vehicles;
+	protected Map<String,Junction> junctionsM;
+	protected Map<String,Road> roadsM;
+	protected Map<String,Vehicle> vehiclesM;
 	
 	RoadMap() {
 	}
 	
 	void addJunction(Junction j) {
-		junct.add(j);
-		junctionMap.entrySet().forEach(null);
+		if(junctionsM.get(j.getId()) != null); //TODO Exception
+
+		junctions.add(j);
+		junctionsM.put(j.getId(), j);
 	}
-	
+
+	void addRoad(Road r){
+		roads.add(r);
+		roadsM.put(r.getId(), r);
+	}
 	void addVehicle(Vehicle v) {
-		ve.add(v);
-		vehiclesMap.entrySet().forEach(null);
-		/*(i) no existe ningún otro vehículo con el mismo identificador; y (ii)
-		el itinerario es válido, es decir, existen carreteras que conecten los cruces consecutivos
-		de su itinerario. En caso de que no se cumplan (i) y (ii), el método debe lanzar una
-		excepción.*/
+		vehicles.add(v);
+		vehiclesM.put(v.getId(), v);
 
 	}
 }
