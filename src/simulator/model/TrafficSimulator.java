a743a976f5d3ddd7e9fd2cff0e6e704de6fff9e2
samuelrdgz <61139116+samuelrdgz@users.noreply.github.com>
2020-04-20 23:47:36 +0200

    trafficsimulator

--- src/simulator/model/TrafficSimulator.java
+++ src/simulator/model/TrafficSimulator.java
@@ -1,9 +1,11 @@
 package simulator.model;
 
+import java.util.List;
+
 import org.json.JSONObject;
 import simulator.misc.SortedArrayList;
 
-public class TrafficSimulator {
+public class TrafficSimulator implements TrafficSimObserver {
     private final RoadMap roadMap;
     private SortedArrayList<Event> eventList;
     private int _time;
@@ -16,39 +18,51 @@ public class TrafficSimulator {
 
     public void addEvent(Event e) {
         eventList.add(e);
+        onEventAdded(roadMap, eventList, e, _time);
     }
 
-    public void advance() {
+    public void advance(){
         _time++;
-
+		onAdvanceStart(roadMap, eventList, _time);
         for (Event e : eventList) {
             if (e.getTime() == _time)
                 e.execute(roadMap);
             //No hace falta eliminar los eventos, dado que _time siempre incrementa.
         }
-
         for (Junction j : roadMap.getJunctions()) {
             j.advance(_time);
         }
-
         for (Road r : roadMap.getRoads()) {
             r.advance(_time);
         }
-
+        onAdvanceEnd(roadMap, eventList, _time);
     }
 
     public void reset() {
         roadMap.reset();
+        onReset(roadMap, eventList, _time);
         eventList = new SortedArrayList<Event>();
         _time = 0;
     }
 
     public JSONObject report() {
-
         JSONObject o = new JSONObject();
         o.put("time", _time);
         o.put("state", roadMap.report());
         return o;
     }
-
+    
+	@Override
+	public void onAdvanceStart(RoadMap map, List<Event> events, int time) {}
+	@Override
+	public void onAdvanceEnd(RoadMap map, List<Event> events, int time) {}
+	@Override
+	public void onEventAdded(RoadMap map, List<Event> events, Event e, int time) {}
+	@Override
+	public void onReset(RoadMap map, List<Event> events, int time) {}
+	@Override
+	public void onRegister(RoadMap map, List<Event> events, int time) {}
+	@Override
+	public void onError(String err) {}
+    
 }
